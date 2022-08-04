f033fad8f2d7476f6cc0a6cacf5277efbb1eaf71
shinya524 <gheena1800@gmail.com>
2022-08-04 01:22:57 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to list_path(list.id)
+    redirect_to '/top'
   end
 
   def index
